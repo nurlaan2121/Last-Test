a60921be3141b8f2d18bb71df4d743ac79ec2a92
Nurislam Toygonbaev <ntoygonbaev098@gmail.com>
2023-12-29 21:49:26 +0600

    last suka

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+
+
+        Person person = new Person();
+        Person person2 = new Person();
+        Person person3 = new Person();
+        person.method();
+        person2.method();
+        person3.method();
     }
 }
